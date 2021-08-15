23e1e2300b9994c8136f32593f6faeba6ede6973
Scherub <steffen.engel@zfx.info>
2021-08-15 09:30:59 +0200

    Added PatchOperationAddOrReplace

---
+++ Source/PatchOperationAddOrReplace.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Xml;
+using Verse;
+
+namespace StonecuttingExtended
+{
+	/// <summary>
+	/// Patch Operation to either Add or Replace a node.
+	/// Unfortunately, the patcher doesn't seem to recognise this class, so it doesn't work for now.
+	/// </summary>
+	public class PatchOperationAddOrReplace : PatchOperationPathed
+	{
+		private XmlContainer value;
+
+		protected override bool ApplyWorker(XmlDocument xml)
+		{
+			XmlNode valueNode = value.node;
+
+			bool result = false;
+
+			foreach (XmlNode xmlNode in xml.SelectNodes(xpath).Cast<XmlNode>().ToArray())
+			{
+				result = true;
+
+				foreach (var valueChildNode in valueNode.ChildNodes.Cast<XmlNode>())
+				{
+					XmlNode xmlChildNode = xmlNode.SelectSingleNode(valueNode.Name);
+
+					if (xmlChildNode == null)
+					{
+						xmlNode.AppendChild(xmlNode.OwnerDocument.ImportNode(valueChildNode, true));
+					}
+					else
+					{
+						xmlNode.InsertBefore(xmlNode.OwnerDocument.ImportNode(valueChildNode, true), xmlChildNode);
+						xmlNode.RemoveChild(xmlChildNode);
+					}
+				}
+			}
+
+			return result;
+		}
+	}
+}
